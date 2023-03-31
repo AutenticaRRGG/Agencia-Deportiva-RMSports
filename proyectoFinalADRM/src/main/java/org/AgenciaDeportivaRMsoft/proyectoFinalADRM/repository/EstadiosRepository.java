9782421bcb98793233dd6fd63574d8c946d4623c
Abraham G <Abraham G@ABRAHAM>
2023-03-30 22:48:32 -0600

    Agregar los repositorios

---
+++ proyectoFinalADRM/src/main/java/org/AgenciaDeportivaRMsoft/proyectoFinalADRM/repository/EstadiosRepository.java
@@ -0,0 +1,8 @@
+package org.AgenciaDeportivaRMsoft.proyectoFinalADRM.repository;
+
+import org.AgenciaDeportivaRMsoft.proyectoFinalADRM.entity.Estadio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadiosRepository extends JpaRepository <Estadio, Integer> {
+
+}
