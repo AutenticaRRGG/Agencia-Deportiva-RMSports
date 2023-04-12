18c2db0061ff097ffa099d76035ca7a311c9bb15
AutenticaRRGG <117795772+AutenticaRRGG@users.noreply.github.com>
2023-04-11 23:49:27 -0600

    Delete proyectoFinalADRM directory

--- proyectoFinalADRM/src/main/java/org/AgenciaDeportivaRMsoft/proyectoFinalADRM/repository/DeportistasRepository.java
+++
@@ -1,7 +0,0 @@
-package org.AgenciaDeportivaRMsoft.proyectoFinalADRM.repository;
-
-import org.AgenciaDeportivaRMsoft.proyectoFinalADRM.entity.Deportista;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeportistasRepository extends JpaRepository <Deportista, Integer> {
-}
--- proyectoFinalADRM/src/main/java/org/AgenciaDeportivaRMsoft/proyectoFinalADRM/repository/PerfilesRepository.java
+++
@@ -1,8 +0,0 @@
-package org.AgenciaDeportivaRMsoft.proyectoFinalADRM.repository;
-
-import org.AgenciaDeportivaRMsoft.proyectoFinalADRM.entity.Perfil;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerfilesRepository extends JpaRepository<Perfil, Integer>{
-
-}
